b64a439e91c3951360f2ce6215ba7e4c19214af6
Goulven <grobodol@gmail.com>
2016-11-25 15:17:26 +0100

    Remove submit button from form class

--- Form/ConversationType.php
+++ Form/ConversationType.php
@@ -17,7 +17,6 @@ class ConversationType extends AbstractType
         $builder
             ->add('subject')
             ->add('firstMessage', PrivateMessageType::class)
-            ->add('save', SubmitType::class, array('label' => 'Send'))
         ;
     }
 
