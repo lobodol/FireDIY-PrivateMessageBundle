97fd7533567dd6daf296eca39be3005d2e98c500
Goulven <grobodol@gmail.com>
2016-11-28 14:17:33 +0100

    Update Conversation's form : add field recipients

--- Form/ConversationType.php
+++ Form/ConversationType.php
@@ -3,21 +3,21 @@
 namespace FireDIY\PrivateMessageBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ConversationType extends AbstractType
 {
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('recipients')
             ->add('subject')
-            ->add('firstMessage', PrivateMessageType::class)
-        ;
+            ->add('firstMessage', PrivateMessageType::class);
     }
 
     /**
